a35cca238dad75402107221a7bef15fae070d3d0
Gaurav Kumar <gauravrajput44@gmail.com>
2024-11-17 15:05:09 +0100

    Add mochawesome reporter and required scripts along with dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,14 @@ module.exports = defineConfig({
     baseUrl: 'https://opensource-demo.orangehrmlive.com',
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/*.cy.js',
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'cypress/reporter.config.json',
+    },
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/reports/screenshots',
+    video: true,
+    videosFolder: 'cypress/reports/videos',
     defaultCommandTimeout: 10000,
     viewportWidth: 1280,
     viewportHeight: 720
